bdc138378eb946670d64a790c143f47c7bb67b43
Josh Duff <me@JoshDuff.com>
2017-08-08 11:45:20 -0500

    Bah indentation

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const escape = require('sqlstring').escape
 
 module.exports = function sqlTag(queryParts, ...values) {
 	return queryParts.reduce((query, queryPart, i) => {
-			const valueExists = i < values.length
-			const text = query + queryPart
+		const valueExists = i < values.length
+		const text = query + queryPart
 
-			return valueExists ? text + jsonCapableEscape(values[i]) : text
-		}, '')
+		return valueExists ? text + jsonCapableEscape(values[i]) : text
+	}, '')
 }
 
 const jsonCapableEscape = str => escape(isObject(str) ? JSON.stringify(str) : str)
