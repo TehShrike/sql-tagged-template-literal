f76b5af37728eb0dc8c3f44b29f3d4f10040948d
Josh Duff <me@JoshDuff.com>
2017-08-08 11:42:08 -0500

    Refactoring for readability

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const escape = require('sqlstring').escape
 
 module.exports = function sqlTag(queryParts, ...values) {
-	return queryParts.reduce((query, queryPart, i) =>
-		`${query}${queryPart}${i < values.length ? jsonCapableEscape(values[i]) : ''}`,
-		'')
+	return queryParts.reduce((query, queryPart, i) => {
+			const valueExists = i < values.length
+			const text = query + queryPart
+
+			return valueExists ? text + jsonCapableEscape(values[i]) : text
+		}, '')
 }
 
-function jsonCapableEscape(str) {
-	return escape(isObject(str) ? JSON.stringify(str) : str)
-}
-
-function isObject(str) {
-	return str && typeof str === 'object'
-}
+const jsonCapableEscape = str => escape(isObject(str) ? JSON.stringify(str) : str)
+const isObject = str => str && typeof str === 'object'
